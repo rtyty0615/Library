8160817d58bc9d8096907a0f41fffc5c26a4dfe4
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-01-21 22:43:56 +0800

    Add a function addBookToPage() to loop through the array and display each book info

--- script.js
+++ script.js
@@ -25,3 +25,29 @@ function addBookToLibrary(title, author, page, read) {
 addBookToLibrary('Founders at Work', 'Jessica Livingston', 506, true);
 addBookToLibrary('My Life and Work', 'Henry Ford', 439, true);
 
+function addBookToPage(){
+    for (const book of myLibrary){
+        const container = document.querySelector('#container');
+        const displayBook = document.createElement("div");
+        displayBook.id = book.id;
+        const title = document.createElement("p");
+        title.textContent = book.title;
+        displayBook.appendChild(title);
+        const author = document.createElement("p");
+        author.textContent = book.author;
+        displayBook.appendChild(author);
+        const page = document.createElement("p");
+        page.textContent = 'Pages: ' + book.page;
+        displayBook.appendChild(page);
+        const read = document.createElement("button");
+        read.textContent = book.read;
+        displayBook.appendChild(read);
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        displayBook.appendChild(deleteButton);
+        container.appendChild(displayBook);
+    }
+}
+
+addBookToPage()
+
