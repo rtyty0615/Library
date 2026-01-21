fe49ae836e02533e43fcbc5d3ea0beda846ba269
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-01-21 20:34:36 +0800

    Create an array myLibrary to store books; function addBookToLibrary

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+const myLibrary = [
+    {
+        title: "Clean Code",
+        author: "Robert C. Martin",
+        pages: 464,
+        read: false
+    },
+];
+
+
+
 function Book(title, author, page, read) {
     this.title = title;
     this.author = author;
     this.page = page;
     this.read = read;
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.page} pages, ${this.read ? 'read' : 'not read'}.`;
-    };
+    this.id = crypto.randomUUID()
 }
 
-const book1 = new Book('Founders at Work', 'Jessica Livingston', 506, true);
-const book2 = new Book('My Life and Work', 'Henry Ford', 439, true);
-book1.info();
-book2.info();
+function addBookToLibrary(title, author, page, read) {
+    let book = new Book(title, author, page, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary('Founders at Work', 'Jessica Livingston', 506, true);
+addBookToLibrary('My Life and Work', 'Henry Ford', 439, true);
+
