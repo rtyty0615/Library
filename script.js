27e30e2c1c6c405202935782725b98c7764a0f54
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-02-08 16:33:57 +0800

    Refactor the codes to use class instead of plain constructors.

--- script.js
+++ script.js
@@ -1,107 +1,106 @@
-const myLibrary = [
-    {
-        title: "Clean Code",
-        author: "Robert C. Martin",
-        page: 464,
-        read: false
-    },
-];
+class Book{
+    constructor(title, author, page, read){
+        this.title = title;
+        this.author = author;
+        this.page = page;
+        this.read = read;
+        this.id = crypto.randomUUID()
+    }
 
-function Book(title, author, page, read) {
-    this.title = title;
-    this.author = author;
-    this.page = page;
-    this.read = read;
-    this.id = crypto.randomUUID()
-}
-
-function addBookToLibrary(title, author, page, read) {
-    let book = new Book(title, author, page, read);
-    myLibrary.push(book);
-}
-
-addBookToLibrary('Founders at Work', 'Jessica Livingston', 506, true);
-addBookToLibrary('My Life and Work', 'Henry Ford', 439, true);
-
-
-const container = document.querySelector('#container');
-
-function addBookToPage(){
-    container.innerHTML = "";
-    let i = 0;
-    for (const book of myLibrary){
-        
-        const displayBook = document.createElement("div");
-        displayBook.id = book.id;
-        const title = document.createElement("p");
-        title.textContent = book.title;
-        title.classList.add("title");
-        displayBook.appendChild(title);
-        const author = document.createElement("p");
-        author.textContent = book.author;
-        author.classList.add("author");
-        displayBook.appendChild(author);
-        const page = document.createElement("p");
-        page.textContent = 'Pages: ' + book.page;
-        page.classList.add("page");
-        displayBook.appendChild(page);
-        const read = document.createElement("button");
-        read.textContent = book.read ? "Status: Read" : "Status: Unread";
-        // toggle
-        read.classList.toggle("read", book.read);
-        read.classList.add("toggle-btn");
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.dataset.index = i;
-        i++;
-        const div = document.createElement("div");
-        div.appendChild(read);
-        div.appendChild(deleteButton);
-        displayBook.appendChild(div);
-        container.appendChild(displayBook);  
+    toggleReadStatus() {
+        this.read = !this.read;
     }
 }
-addBookToPage()
 
-
-const dialog = document.querySelector("dialog");
-const newBookBtn = document.querySelector('#new-book');
-newBookBtn.addEventListener('click', () => {
-    dialog.showModal();
-});
-
-// const submitBtn = document.querySelector('#submit-btn');
-const submitForm = document.querySelector('form');
-submitForm.addEventListener("submit", () => {
-    const inputTitle = document.querySelector('#title');
-    const title = inputTitle.value;
-    const inputAuthor = document.querySelector('#author');
-    const author = inputAuthor.value;
-    const inputPage = document.querySelector('#page');
-    const page = inputPage.value;
-    const inputRead = document.querySelector('#read');
-    const read = inputRead.checked;
-    console.log(title, author, page, read);
-    addBookToLibrary(title, author, page, read);
-    addBookToPage();
-    submitForm.reset();
-});
-
-const cancelBtn = document.querySelector('#cancel-btn');
-cancelBtn.addEventListener("click", () => {
-    submitForm.reset();
-    dialog.close();
-})
-
-container.addEventListener("click", (e) => {
-    if(e.target.hasAttribute('data-index')){
-        const indexToRemove = e.target.dataset.index; 
-        myLibrary.splice(indexToRemove, 1);
-        addBookToPage();
+class Library {
+    constructor(){
+        this.books = [];
+        this.bindEvents();
     }
-    if (e.target.classList.contains('toggle-btn')) {
-        const readBtn = e.target;
-        const isRead = readBtn.classList.toggle('read');
-        readBtn.textContent = isRead ? "Status: Read" : "Status: unread";
+
+    addBook(title, author, page, read){
+        const newBook = new Book(title, author, page, read);
+        this.books.push(newBook);
+        this.render(); 
     }
-})
+
+    render(){
+        const container = document.querySelector('#container');
+        container.innerHTML = "";
+        this.books.forEach((book, index) => {
+            const displayBook = document.createElement("div");
+            displayBook.id = book.id;
+            const title = document.createElement("p");
+            title.textContent = book.title;
+            title.classList.add("title");
+            displayBook.appendChild(title);
+            const author = document.createElement("p");
+            author.textContent = book.author;
+            author.classList.add("author");
+            displayBook.appendChild(author);
+            const page = document.createElement("p");
+            page.textContent = 'Pages: ' + book.page;
+            page.classList.add("page");
+            displayBook.appendChild(page);
+            const readButton = document.createElement("button");
+            readButton.textContent = book.read ? "Status: Read" : "Status: Unread";
+            readButton.classList.toggle("read", book.read);
+            readButton.classList.add("toggle-btn");
+            readButton.onclick = () => {
+                book.toggleReadStatus();
+                this.render();
+            }
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.onclick = () => this.removeBook(index);
+            const div = document.createElement("div");
+            div.appendChild(readButton);
+            div.appendChild(deleteButton);
+            displayBook.appendChild(div);
+            container.appendChild(displayBook);  
+        });
+    }
+    removeBook(index) {
+        this.books.splice(index, 1);
+        this.render();
+    }
+
+    bindEvents() {
+        const dialog = document.querySelector("dialog");
+        const newBookBtn = document.querySelector('#new-book');
+        newBookBtn.addEventListener('click', () => {
+            dialog.showModal();
+        });
+
+        const submitForm = document.querySelector('form');
+        submitForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const inputTitle = document.querySelector('#title');
+            const title = inputTitle.value;
+            const inputAuthor = document.querySelector('#author');
+            const author = inputAuthor.value;
+            const inputPage = document.querySelector('#page');
+            const page = inputPage.value;
+            const inputRead = document.querySelector('#read');
+            const read = inputRead.checked;
+            this.addBook(title, author, page, read);
+            submitForm.reset();
+            dialog.close();
+        });
+
+        const cancelBtn = document.querySelector('#cancel-btn');
+        cancelBtn.addEventListener("click", () => {
+            submitForm.reset();
+            dialog.close();
+        })
+    }
+
+}
+
+const myLibrary = new Library();
+myLibrary.addBook('Founders at Work', 'Jessica Livingston', 506, true);
+myLibrary.addBook('My Life and Work', 'Henry Ford', 439, true);
+myLibrary.addBook("Clean Code", "Robert C. Martin", 464, false);
+
+
+
